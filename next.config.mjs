bb21af11c2b03942b466bde54a7b643ed018588a
James Conte <James@mld.com>
2025-02-18 15:47:34 -0700

    inital commits 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    extends: ["next/core-web-vitals"],
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {
+      // Define any custom ESLint rules you want.
+    },
+  };
+  
